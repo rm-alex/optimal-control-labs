c7374b9dbdbb9b0631f6f8c1df213e25fd6b41fa
Alexey Rumyantsev <alexeyrumyantsev04@gmail.com>
2025-11-30 12:36:41 +0300

    com

--- lab6/src/scripts/code.m
+++ lab6/src/scripts/code.m
@@ -14,3 +14,21 @@ P=are(A,B*B'-g^(-2)*Bf*Bf',Q)
 K=-B'*P
 eP=eig(P)
 eK=eig(A+B*K)
+
+%% H_infty-norm
+C1=[1 0];
+C2=[0 1];
+
+Acl=A+B*K;
+
+sys1ss=ss(Acl,Bf,C1,0);
+% sys1tf=tf(sys1ss);
+[ninf1,fpeak1]=hinfnorm(sys1ss)
+
+sys2ss=ss(Acl,Bf,C2,0);
+% sys2tf=tf(sys2ss);
+[ninf2,fpeak2]=hinfnorm(sys2ss)
+
+sys3ss=ss(Acl,Bf,eye(2),0);
+% sys3tf=tf(sys3ss);
+[ninf3,fpeak3]=hinfnorm(sys3ss)
